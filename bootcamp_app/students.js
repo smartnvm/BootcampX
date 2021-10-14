796bc037c35c3fc064ff420cdfa1e0bbc97e729e
AJ <aj@smartnvm.com>
2021-10-14 05:48:25 +0000

    - parametrized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,19 +9,22 @@ const pool = new Pool({
 
 args = process.argv.slice(2);
 
-cohortName = args[0];
+cohortName = args[0] || 'FEB';
 limit = args[1] || 5;
 
-console.log(cohortName, limit);
-pool.query(
+const strQuery =
   `select students.id, students.name, cohorts.name as cohorts_name
   from students 
   join cohorts on cohorts.id = students.cohort_id
   where cohorts.name like $1
-  limit $2;`, [cohortName, limit])
+  limit $2;`;
+
+const values = [`%${cohortName}%`, limit];
+
+pool.query(strQuery, values)
   .then((res) => {
-    res.rows.forEach((user) => {
-      console.log(`${user}`);
+    res.rows.forEach((row) => {
+      console.log(row);
     });
   }).catch((err) => console.error('query error', err.stack))
   .finally(() => pool.end());
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -13,15 +13,18 @@ cohortName = args[0] || 'JUL02';
 limit = args[1] || 10;
 
 console.log(cohortName, limit);
-pool.query(
+const strQuery =
   `SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM assistance_requests
   JOIN teachers ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name = $1
+  WHERE cohorts.name LIKE $1
   ORDER BY teachers.name
-  LIMIT $2;`, [cohortName, limit])
+  LIMIT $2;`;
+
+const values = [`%${cohortName}%`, limit];
+pool.query(strQuery, values)
   .then((res) => {
     res.rows.forEach((row) => {
       console.log(`${row.cohort}: ${row.teacher}`);
